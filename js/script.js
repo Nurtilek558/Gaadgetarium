1f1aada2006273b9ee5500a1259171cef44230f6
Nurtilek558 <navatovnurtilek105@gmail.com>
2025-04-24 17:04:38 +0600

    dron

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    loop:true,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
